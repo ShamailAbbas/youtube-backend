016959b2a852531753e4e1e792daf4a27cdb5059
unknown <Shamail>
2021-05-16 16:28:58 +0500

    make it better

--- routes/users.js
+++ routes/users.js
@@ -1,46 +1,46 @@
-const express = require('express')
-const router = express.Router()
-const { User } = require('../models/User')
+const express = require("express");
+const router = express.Router();
+const { User } = require("../models/User");
 
 //=================================
 //             User
 //=================================
 
-router.post('/register', (req, res) => {
-  const user = new User(req.body)
-
+router.post("/register", (req, res) => {
+  const user = new User(req.body);
+  console.log("you hitted /register route");
   user.save((err, doc) => {
-    if (err) return res.json({ success: false, err })
+    if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
-    })
-  })
-})
+    });
+  });
+});
 
-router.post('/login', (req, res) => {
-  console.log('login route received')
+router.post("/login", (req, res) => {
+  console.log("login route received");
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user)
       return res.json({
         loginSuccess: false,
-        message: 'Auth failed, email not found',
-      })
+        message: "Auth failed, email not found",
+      });
 
     user.comparePassword(req.body.password, (err, isMatch) => {
       if (!isMatch)
-        return res.json({ loginSuccess: false, message: 'Wrong password' })
+        return res.json({ loginSuccess: false, message: "Wrong password" });
 
       user.generateToken((err, user) => {
-        if (err) return res.status(400).send(err)
+        if (err) return res.status(400).send(err);
 
         res.status(200).json({
           loginSuccess: true,
           userId: user._id,
           user,
-        })
-      })
-    })
-  })
-})
+        });
+      });
+    });
+  });
+});
 
-module.exports = router
+module.exports = router;
